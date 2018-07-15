2dec2c78df9fa8bdc17c46068cfa1e55c70868d5
Tina <tparkmedia@gmail.com>
2018-07-15 13:54:54 -0700

    added comments in server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const 
+const
   express = require('express'),
-  app = express(), 
+  app = express(),
   logger = require('morgan'),
   mongoose = require('mongoose'),
-  PORT = 3000, 
+  PORT = 3000,
   haikuRouter = require('./routers/haikuRouter')
 
+//  connect to db
 mongoose.connect('mongodb://localhost/haiku', (err) => {
   console.log(err || "Successfully connectedd to MONGOD")
 })
 
+
+// middleware
 app.use(logger('dev'))
 app.use(express.json())
 
+// router
 app.use('/api/haikus', haikuRouter)
 
 app.listen(PORT, (err) => {
